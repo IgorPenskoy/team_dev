e0bb48bb01397afbae57581ca8774f6ef39f0d59
igorpenskoy <igorpenskoy@yandex.ru>
2017-03-24 09:36:00 +0300

    Add transport problem class

---
+++ problem.py
@@ -0,0 +1,16 @@
+class TransportProblem:
+    def __init__(self):
+        self.common_costs = None
+        self.customers = None
+        self.graph = None
+        self.providers = None
+        self.table = None
+
+    def check_closeness(self):
+        return True
+    
+    def correct_closeness(self, rates):
+        pass
+
+    def solve(self, rates, method):
+        return 0
