b0ca8a8934c3491130dec21968c0325967ab3341
igorpenskoy <igorpenskoy@yandex.ru>
2017-03-30 18:21:18 +0300

    Add other methods for problem class. Delete graph from problem class, must be in gui

--- problem.py
+++ problem.py
@@ -4,18 +4,38 @@ class Problem:
 		self.providers = providers
 		self.rates = rates
 		self.common_costs = None
-		self.graph = None
 		self.table = None
-		self.create_table()
+		self.build_table()
 
 	def solve(self):
 		return 0
 		
-	def create_table(self):
+	def build_table(self):
 		pass
-		
+	
+	def make_closeness(self):
+		pass
+
 	def check_closeness(self):
 		return True
-
-	def correct_closeness(self):
+		
+	def make_basic_plan(self):
 		pass
+
+	def fix_degeneracy(self):
+		pass
+		
+	def check_degeneracy(self):
+		return False
+
+	def make_optimality(self):
+		pass
+	
+	def check_optimality(self):
+		return True
+		
+	def get_plan_potentials(self):
+		pass
+		
+	def get_expenses(self):
+		return 0
