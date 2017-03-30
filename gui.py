73ddee162926aead6d7570c7ff421f0a3f57da94
ilya27ilya <777@MacBook-Pro-777.local>
2017-03-30 21:26:37 +0300

    Add gui carcass class

---
+++ gui.py
@@ -0,0 +1,12 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(640, 480)
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Transportation problem"))
