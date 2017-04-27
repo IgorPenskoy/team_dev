0d885f75fb5b9cc80b741c1207b85dda388222f0
ilya27ilya <ilya22227@gmail.com>
2017-04-27 13:01:40 +0300

    Modify tests for gui data input #issue_10

--- test_gui.py
+++ test_gui.py
@@ -1,13 +1,30 @@
-import unittest
-import sys
+from pytestqt import qtbot
 from gui import *
 
-class TestGui(unittest.TestCase):
-	def test_setupUi(self):
-		pass
+def test_blank_data(qtbot):
+	MainWindow = QtWidgets.QMainWindow()
+	ui = Ui_MainWindow()
+	ui.setupUi(MainWindow)
+	qtbot.addWidget(ui)
+	qtbot.mouseClick(ui.calculation_pushbutton, QtCore.Qt.LeftButton)
+	assert ui.alert_label.text() == 'НЕКОРРЕКТНЫЕ ДАННЫЕ'
 
-	def test_retranslateUi(self):
-		pass
+def test_not_digit_data(qtbot):
+	MainWindow = QtWidgets.QMainWindow()
+	ui = Ui_MainWindow()
+	ui.setupUi(MainWindow)
+	qtbot.addWidget(ui)
+	qtbot.mouseClick(ui.random_pushbutton, QtCore.Qt.LeftButton)
+	ui.rates_tablewidget.setItem(0, 0, QtWidgets.QTableWidgetItem(str('asasdasd')))
+	qtbot.mouseClick(ui.calculation_pushbutton, QtCore.Qt.LeftButton)
+	assert ui.alert_label.text() == 'НЕКОРРЕКТНЫЕ ДАННЫЕ'
 
-if __name__ == '__main__':
-    unittest.main()
+def test_negative_data(qtbot):
+	MainWindow = QtWidgets.QMainWindow()
+	ui = Ui_MainWindow()
+	ui.setupUi(MainWindow)
+	qtbot.addWidget(ui)
+	qtbot.mouseClick(ui.random_pushbutton, QtCore.Qt.LeftButton)
+	ui.rates_tablewidget.setItem(0, 0, QtWidgets.QTableWidgetItem(str(-1)))
+	qtbot.mouseClick(ui.calculation_pushbutton, QtCore.Qt.LeftButton)
+	assert ui.alert_label.text() == 'НЕКОРРЕКТНЫЕ ДАННЫЕ'
